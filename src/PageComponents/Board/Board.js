38b81f4e2f5f69d5faa5cf1280ef791c3fd8744d
ComittedComitter <65293328+ComittedComitter@users.noreply.github.com>
2020-06-28 21:44:53 +0200

    meet a board styling

--- src/PageComponents/Board/Board.js
+++ src/PageComponents/Board/Board.js
@@ -11,15 +11,17 @@ export default function Board() {
         <Card style={{ fontSize: 20 }}>
           <Card.Header>Meet a director</Card.Header>
           <Card.Body>
-            <Card.Title>Davis Escudero</Card.Title>
+            <Card.Title>David Escudero</Card.Title>
             <Card.Text>
               To continue the Meet a director event, we would like to introduce
               you to this month's member: David Escudero. We know David as our
               Senior Vice President of the Gogle Science branch but he is much
               more than that! For example, David is what is known as a
-              “trekkie”, a Star Trek fan. He is known around the office for his
-              Star Trek mugs and his favourite character is Spock! David also
-              has a dog named Chewie, quite a name for a Star Trek fan...
+              <span class="redText"> “trekkie”</span>, a Star Trek fan. He is
+              known around the office for his Star Trek mugs and his favourite
+              character is Spock! David also has a{" "}
+              <span class="redText">dog named Chewie</span>, quite a name for a
+              Star Trek fan...
             </Card.Text>
           </Card.Body>
         </Card>
@@ -37,10 +39,11 @@ export default function Board() {
               little glimps of who the team consists of. We will kick things off
               with introducing Barissa Keita. This wonderfull person is, next to
               being an amazing collegue, our Public Affairs Officer. She is
-              driven and carreer focussed but never fails to make time for her
-              other passion: sports! She plays tennis on a regular basis and
-              golfs every now and then. She is also an avid cross-fitter, and
-              always up for a friendly competition.
+              driven and <span class="greenText">carreer focussed</span> but
+              never fails to make time for her other passion:{" "}
+              <span class="greenText">sports!</span> She plays tennis on a
+              regular basis and golfs every now and then. She is also an avid
+              cross-fitter, and always up for a friendly competition.
             </Card.Text>
           </Card.Body>
         </Card>
